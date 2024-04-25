0f6aeb202a534ab46c9a60b94fb0fcd8ed3f443a
James Rhodes <james.rhodes@hotmail.com.au>
2024-04-26 08:36:15 +1000

    Feat: Started the actual animation finally

--- adding-sines/src/main.rs
+++ adding-sines/src/main.rs
@@ -1,13 +1,10 @@
-use macroquad::prelude::*;
 use std::f32::consts::PI;
 
-pub mod animation;
-use animation::{
-    ui::{AxisStyle, Graph, GraphEndPointStyle, GraphStyle, LabelStyle, TickStyle},
+use macroquad::prelude::*;
+use mqanim::{
+    plot::{Graph, GraphStyle, LabelStyle, MarkerStyle},
     Animation,
 };
-
-use crate::animation::ui::MarkerStyle;
 
 const WINDOW_WIDTH: f32 = 1280.0;
 const WINDOW_HEIGHT: f32 = 720.0;
@@ -21,87 +18,60 @@ fn window_conf() -> Conf {
     }
 }
 
+enum EnabledFreqs {
+    OneHz,
+    TwoHz,
+    ThreeHz,
+    FourHz,
+    FiveHz,
+}
+
+const RESOLUTION: u8 = 200;
 #[macroquad::main(window_conf)]
 async fn main() {
     let mut animation = Animation::new(WINDOW_WIDTH, WINDOW_HEIGHT, None);
-    // animation.enable_fxaa();
+    animation.enable_fxaa();
 
-    let x_sine: Vec<f32> = (-100..100)
-        .map(|val| animation::map(val as f32, -100.0, 100.0, -2. * PI, 2. * PI))
-        .collect();
-    let y_sine: Vec<f32> = (-100..100)
+    let graph = Graph::new(
+        vec2(0., 0.),
+        vec2(WINDOW_WIDTH - 100., WINDOW_HEIGHT - 100.),
+        -1.0..1.,
+        -3.0..3.,
+    )
+    .style(GraphStyle {
+        x_style: mqanim::plot::AxisStyle {
+            tick_step: 1.,
+            tick_style: mqanim::plot::TickStyle::LabelAndMarker {
+                label_style: LabelStyle::default(),
+                marker_style: MarkerStyle::default(),
+            },
+            ..Default::default()
+        },
+        y_style: mqanim::plot::AxisStyle {
+            tick_step: 1.,
+            tick_style: mqanim::plot::TickStyle::LabelAndMarker {
+                label_style: LabelStyle::default(),
+                marker_style: MarkerStyle::default(),
+            },
+            ..Default::default()
+        },
+    });
+
+    let plot_to_copy: Vec<Vec2> = (0..RESOLUTION)
         .map(|val| {
-            let t = animation::map(val as f32, -100.0, 100.0, -2. * PI, 2. * PI);
-            f32::sin(t)
+            let t = mqanim::map(val as f32, 0., RESOLUTION as f32, -1., 1.);
+            let v = f32::sin(2. * PI * 1. * t)
+                + f32::sin(2. * PI * 3. * t)
+                + f32::sin(2. * PI * 4. * t);
+
+            vec2(t, v)
         })
         .collect();
 
-    let mut time = 0.;
     loop {
-        println!("FPS: {}", macroquad::time::get_frame_time());
-        time += 0.01;
-        let sine: Vec<Vec2> = (-500..500)
-            .map(|val| {
-                let t = animation::map(val as f32, -100.0, 100.0, -2. * PI, 2. * PI);
-                vec2(t, -f32::sin(t + time))
-            })
-            .collect();
         animation.set_camera();
-        let _mouse = animation.get_world_mouse();
-
-        let graph = Graph::new(
-            vec2(0., 0.),
-            vec2(WINDOW_WIDTH - 100., WINDOW_HEIGHT - 100.),
-            -3.5..3.5,
-            -3.5..3.5,
-        )
-        .style(GraphStyle {
-            x_style: AxisStyle {
-                tick_step: 0.5,
-                tick_style: TickStyle::LabelAndMarker {
-                    label_style: LabelStyle {
-                        pos_offset: vec2(0., 0.),
-                        color: WHITE,
-                        font_size: 12,
-                        decimal_places: 2,
-                    },
-                    marker_style: MarkerStyle {
-                        length: 5.,
-                        thickness: 2.,
-                        color: WHITE,
-                    },
-                },
-                end_point_style: GraphEndPointStyle::Arrow { thickness: 7. },
-                line_thickness: 3.,
-                line_color: WHITE,
-            },
-            y_style: AxisStyle {
-                tick_step: 0.5,
-                tick_style: TickStyle::LabelAndMarker {
-                    label_style: LabelStyle {
-                        pos_offset: vec2(0., 0.),
-                        color: WHITE,
-                        font_size: 12,
-                        decimal_places: 2,
-                    },
-                    marker_style: MarkerStyle {
-                        length: 5.,
-                        thickness: 2.,
-                        color: WHITE,
-                    },
-                },
-                end_point_style: GraphEndPointStyle::Arrow { thickness: 7. },
-                line_thickness: 3.,
-                line_color: WHITE,
-            },
-        });
         graph.draw_axes();
-        graph.plot_line_vec(&sine, 3., PURPLE);
-        graph.plot_line_xy(&x_sine, &y_sine, 3., BLUE);
-        let pt = graph.graph_to_world(vec2(-0.5, -0.5));
-        draw_circle(pt.x, pt.y, 10., ORANGE);
-        graph.plot_pt_vec(&vec2(0.2, 0.2), 10., RED);
-        graph.plot_pt_xy(0.4, 0.4, 15., BLUE);
+        graph.plot_line_vec(&plot_to_copy, 2., ORANGE);
 
         animation.set_default_camera();
         animation.draw_frame();
